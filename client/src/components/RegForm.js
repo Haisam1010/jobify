726be1e12c6f3cbf5606fafbdeb57821d1766aef
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-07 17:23:15 +0530

    Created Component Level Reg-Form

---
+++ client/src/components/RegForm.js
@@ -0,0 +1,19 @@
+
+
+const RegForm = ({value,handleChange,name,type,labelText}) => {
+  return (
+    <div className="form-row">
+    <label htmlFor={name} className="form-label" >
+        {labelText || name}
+    </label>
+    <input 
+     type={type} 
+     value={value.name}
+     onChange={handleChange} 
+     name='name' 
+     className='form-input' />
+</div>
+  )
+}
+
+export default RegForm
