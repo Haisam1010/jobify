c5115fd9f3641664a81969dbbcf3299793c3e61c
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-09 10:58:55 +0530

    added clear reducer

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,4 +1,29 @@
+import { DISPLAY_ALERT,CLEAR_ALERT } from "./action"
+
 const reducer = (state,action) =>{
+
+    if(action.type === DISPLAY_ALERT){
+        return{
+            ...state,
+            showAlert : true,
+            alertType: 'danger',
+            alertText: 'Please Provide All Values '
+
+        }
+    }
+    if(action.type === CLEAR_ALERT){
+        return{
+            ...state,
+            showAlert : false,
+            alertType: '',
+            alertText: ''
+
+        }
+    }
+
+
+
+
     throw new Error (`no such action :${action}`)
 }
 
