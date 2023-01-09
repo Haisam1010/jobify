e401142da28bf24d59d21277037385f31397f3b3
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-09 10:57:26 +0530

    implimented dispatch clear alert function

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,7 @@
-
 import React,{ useState,useReducer,useContext } from "react";
+import reducer from "./reducer";
+import { DISPLAY_ALERT,CLEAR_ALERT } from "./action"
+
 
 const initialState = {
     isLoading:false,
@@ -8,13 +10,24 @@ const initialState = {
     alertType:'',
 }
 
-const AppContext =React.createContext()
+const AppContext = React.createContext()
 
 const AppProvider = ({children}) =>{
-    const [state,setState,]= useState(initialState)
+    const [state,dispatch,]= useReducer(reducer,initialState)
+
+    const displayAlert = ()=>{
+        dispatch({type:DISPLAY_ALERT})
+        clearAlert()
+    }
+
+    const clearAlert = () =>{
+        setTimeout(()=>{
+            dispatch({type:CLEAR_ALERT})
+        },2000)
+    }
 
     return(
-        <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
+        <AppContext.Provider value={{...state,displayAlert}}>{children}</AppContext.Provider>
     )
 }
 
