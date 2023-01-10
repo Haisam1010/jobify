1b316bf99dc584c1cb22353cab45b419c48277c5
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-10 22:19:41 +0530

    created user schema

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const UserSchema = new mongoose.Schema({
     name:
@@ -14,13 +15,34 @@ const UserSchema = new mongoose.Schema({
     {
     type:String, 
     required:[true,'Please Add Email'],
+    validate:{
+        validator:validator.isEmail,
+        message: 'Please Provide Valid Email'
+    },
     unique:true,
     },
-    
+
     password:
     {
     type:String, 
-    required:[true,'Please Add Email'],
-    unique:true,
-    }
+    required:[true,'Please Add Password'],
+    minlenght:6,
+    },
+
+    lastName:
+    {
+    type:String, 
+    maxlenght:20,
+    default: '',
+    trim:true
+    },
+
+    location:
+    {
+    type:String, 
+    maxlenght:20,
+    default: 'my city',
+    trim:true
+    },
 })
+export default mongoose.model('User',UserSchema)
