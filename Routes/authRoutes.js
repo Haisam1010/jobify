c07c1d7ecdcfc42fb1d56e68b979890e83f4a419
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-10 17:04:15 +0530

    Created Routes

---
+++ Routes/authRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import {Register,Login,Update} from '../controllers/authController.js'
+
+const router = express.Router()
+
+router.route('/register').post(Register)
+router.route('/login').post(Login)
+router.route('/updateUser').patch(Update)
+
+export default router
