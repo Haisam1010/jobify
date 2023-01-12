4e4195fca5d3137b9c34268e2fe91ce5f7a24c47
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-12 23:22:39 +0530

    reinitialized

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,15 +3,15 @@ import {StatusCodes} from 'http-status-codes'
 const errorHandler = (err,req,res,next) => {
     console.log(err)
     const dflterror = {
-        statuscode : StatusCodes.INTERNAL_SERVER_ERROR,
+        statusCode : StatusCodes.INTERNAL_SERVER_ERROR,
         msg : 'Something Thing Went Wrong ... ',
     }
    if(err.name === 'ValidatorError'){
-        dflterror.statuscode = StatusCode.BAD_REQUEST
-        // dflterror.err = object.values(err.errors).map((item)=>item.msg).join(',')
-        dflterror.msg = err.msg
+        dflterror.statusCode = StatusCode.BAD_REQUEST
+         dflterror.err = object.values(err.errors).map((item)=>item.msg).join(',')
+        // dflterror.msg = err.msg
      }
     // res.status(dflterror.statuscode).json({msg:err})
-     res.status(dflterror.statuscode).json({msg: dflterror.msg})
+     res.status(dflterror.statusCode).json({msg: dflterror.msg})
 }
 export default errorHandler
