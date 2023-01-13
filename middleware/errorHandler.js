c11583f790248b35295c1bf4c71db2309a0b72b0
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-13 11:57:10 +0530

    reinitialized

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,15 +3,22 @@ import {StatusCodes} from 'http-status-codes'
 const errorHandler = (err,req,res,next) => {
     console.log(err)
     const dflterror = {
-        statusCode : StatusCodes.INTERNAL_SERVER_ERROR,
+        StatusCodes : StatusCodes.INTERNAL_SERVER_ERROR,
         msg : 'Something Thing Went Wrong ... ',
     }
-   if(err.name === 'ValidatorError'){
-        dflterror.statusCode = StatusCode.BAD_REQUEST
-         dflterror.err = object.values(err.errors).map((item)=>item.msg).join(',')
+        if(err.name === 'ValidatorError'){
+        dflterror.StatusCodes = StatusCodes.BAD_REQUEST
+         dflterror.err = Object.values(err.errors).map((item)=>item.msg).join(',')
         // dflterror.msg = err.msg
      }
-    // res.status(dflterror.statuscode).json({msg:err})
-     res.status(dflterror.statusCode).json({msg: dflterror.msg})
+
+        if(err.code && err.code === 11000){
+
+        dflterror.StatusCodes = StatusCodes.BAD_REQUEST
+        dflterror.msg = `${Object.keys(err.keyValue)} Field Has To Be Unique`
+        }
+
+    res.status(dflterror.StatusCodes).json({msg:err})
+    //  res.status(dflterror.statusCode).json({msg: dflterror.msg})
 }
 export default errorHandler
