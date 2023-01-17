54babef243d457b9f5e591b1c6371f74422aa827
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-17 20:49:52 +0530

    Reinitialized

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,16 @@
 import UserModel from "../models/UserModel.js"
 import {StatusCodes} from 'http-status-codes'
+import {BadRequest} from '../Errors/index.js'
 
 const Register = async(req,res) =>{
         const {name,email,password} = req.body
+
         if(!name || !email || !password){
-            throw new Error('Please Provide All Values')
+            throw new BadRequest('Please Provide All Values')
         }
-        const user = await UserModel.create(req.body)
-        res.status(StatusCodes.OK).json({msg:user})
+
+        const user = await UserModel.create({name,email,password})
+        res.status(StatusCodes.OK).json({user})
   
 }
 const Login = async(req,res) =>{
