6f471cd081a3befbf34485c03f589cb14b4f3570
Zaxbee <mohamedhaisam199@gmail.com>
2023-01-31 23:13:41 +0530

    created user register function globaly

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,7 @@
 import React,{ useState,useReducer,useContext } from "react";
 import reducer from "./reducer";
+import axios from 'axios'
+
 import { DISPLAY_ALERT,CLEAR_ALERT,
     REGISTER_USER_BEGIN,
     REGISTER_USER_SUCCESS 
@@ -13,7 +15,9 @@ const initialState = {
     alertType:'',
     user: null,
     token: null,
-    userLocation : ''
+    location: '',
+    Joblocation : '',
+    
 }
 
 const AppContext = React.createContext()
@@ -35,7 +39,25 @@ const AppProvider = ({children}) =>{
     }
 
     const registerUser = async (currentUser)=>{
-        console.log(currentUser)
+        dispatch({type:REGISTER_USER_BEGIN})
+        try {
+            const response = await axios.post('/api/v1/auth/register',currentUser)
+
+            console.log(response);
+
+            const {user,token,location} = response.data
+            dispatch({
+                type:REGISTER_USER_SUCCESS,
+                payload: {user,token,location}
+            })
+            
+        } catch (error) {
+            console.log(error.response)
+            dispatch({
+                type: REGISTER_USER_ERROR, payload: {msg:error.response.data.msg}
+            })
+        }
+        clearAlert()
     }
 
     return(
