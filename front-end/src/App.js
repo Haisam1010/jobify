70a01b083e989debce9c8200ec8f9ba9e4933f8d
Haisam <mohamedhaisam199@gmail.com>
2023-05-03 12:44:12 +0530

    added all nested screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,23 @@
-import {Register,Error,Landing,Dashboard} from "../src/screens" 
+import {Register,Error,Landing} from "../src/screens" 
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {
+  Stats,
+  Shared,
+  Profile,
+  AllJobs,
+  AddJob
+  } from '../src/screens/dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/" element={<Shared/>}>
+      <Route path="stats" element={<Stats/>}/>
+      <Route path="all-jobs" element={<AllJobs/>}/>
+      <Route path="add-jobs" element={<AddJob/>}/>
+      <Route path="profile" element={<Profile/>}/>
+      </Route>
       <Route path="/landing" element={<Landing />}></Route>
       <Route path="/register" element={<Register />}></Route>
       <Route path="*" element={<Error />}></Route>
