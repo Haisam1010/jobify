0d7a4347aae049bd22b3a9bf5228b0beb55cc2f5
Haisam <mohamedhaisam199@gmail.com>
2023-05-09 23:07:49 +0530

    added profile Edit Row And function

--- front-end/src/screens/dashboard/Profile.js
+++ front-end/src/screens/dashboard/Profile.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import { useState } from 'react'
+import Wrapper from '../../assets/wrapper/DashBoardPage'
+import {FormRow,Alert} from '../../components'
+import { useAppContext } from '../../context/appContext'
+
 
 const Profile = () => {
+    const {showAlert,user,displayAlert,updateUser,isLoading} = useAppContext()
+
+    const [name,setName] = useState(user?.name)
+    const [email,setEmail] = useState(user?.email)
+    const [lastname,setLastname] = useState(user?.lastName)
+    const [location,setLocation] = useState(user?.location)
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        if(!name || !email || !lastname || !location){
+          displayAlert()
+          return 
+        }
+        updateUser({name,email,lastname,location})
+    }
   return (
-    <h1>
-      Profile
-    </h1>
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit} >
+        <h3>Profile</h3>
+        {showAlert && <Alert />}
+      <div className='form-center'>
+        <FormRow 
+        type='text'
+        name='name' 
+        value={name}
+        handleChange={(e)=>setName(e.target.value)} 
+        />
+        <FormRow 
+        type='text'
+        name='lastName' 
+        value={lastname}
+        handleChange={(e)=>setLastname(e.target.value)} 
+        />
+        <FormRow 
+        type='text'
+        name='location' 
+        value={location}
+        handleChange={(e)=>setLocation(e.target.value)} 
+        />
+        <FormRow 
+        type='email'
+        name='email' 
+        value={email}
+        handleChange={(e)=>setEmail(e.target.value)} 
+        />
+
+      <button className='btn btn-block' type='submit' disabled={isLoading}> 
+      {isLoading ? 'Please Wait...' : 'save changes'}
+      </button>
+
+      </div>
+     
+      </form>
+    </Wrapper>
   )
 }
 
