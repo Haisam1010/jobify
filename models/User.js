d193311008f6b3d77e7a5dbfeac6ddcf98c62be3
Haisam <mohamedhaisam199@gmail.com>
2023-05-09 12:39:12 +0530

    removed curly braces

--- models/User.js
+++ models/User.js
@@ -48,11 +48,12 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save',async function(){
   
-  if(!this.isModified('password')){
-    return
-  }
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password,salt)
+  if(!this.isModified('password')) return
+    
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
+  
+ 
 })
 
 userSchema.methods.createJWT = function () {
