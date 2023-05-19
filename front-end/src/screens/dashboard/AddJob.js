a102f8a9a711dce41270006e474dbf973f06052c
Haisam <mohamedhaisam199@gmail.com>
2023-05-19 10:38:02 +0530

    created job select vaalue

--- front-end/src/screens/dashboard/AddJob.js
+++ front-end/src/screens/dashboard/AddJob.js
@@ -1,10 +1,78 @@
 import React from 'react'
+import {FormRow,Alert,FormRowSelect} from '../../components'
+import { useAppContext } from '../../context/appContext'
+import Wrapper from '../../assets/wrapper/DashBoardPage'
 
 const AddJob = () => {
+  const {
+    isLoading,
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    handleChange,
+    clearValues,
+    createJob,
+    editJob,
+  } = useAppContext()
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!position || !company || !jobLocation) {
+      displayAlert()
+      return
+    }
+    console.log('create Job')
+  }
+
+  const handleChangeinput = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+
+    console.log(`${name}: ${value}`)
+  }
   return (
-    <h1>
-      Add Job Page
-    </h1>
+    <Wrapper>
+        <form className='form'>
+          <h3>{isEditing ? 'Edit Job' : 'Add Job'}</h3>
+          {showAlert && <Alert />}
+          <div className='form-center'>
+            <FormRow text="text" name="Position" value={position} handleChange={handleChangeinput} ></FormRow>
+            <FormRow text="text" name="company" value={company} handleChange={handleChangeinput} ></FormRow>
+            <FormRow text="text" labelText='Job Location' name="jobLocation" value={jobLocation} handleChange={handleChangeinput} ></FormRow> 
+          
+          {/* Job Type */}
+          <div className='form-row'>
+              <label htmlFor='jobType' className='form-label'>
+                Job Type
+              </label>
+          <select name='jobType' value={jobType} onChange={handleChangeinput} className='form-select'>
+            {jobTypeOptions.map((itemValue,index)=>{
+              return(
+                <option key={index} value={itemValue}>
+                  {itemValue}
+                </option>
+              )
+            })}
+          </select>
+          </div>
+          
+          <div className='btn-container'>
+              <button type='submit' onClick={handleSubmit} className='btn btn-block submit-btn'>
+              Submit Button
+              </button>
+          </div>
+          </div>
+        </form>
+
+    </Wrapper>
   )
 }
 
